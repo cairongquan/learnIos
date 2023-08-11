95fd535cd8a46eb82bf37960aeda2359e2da38b1
蔡荣诠 <cairq@tongbaninfo.com>
2023-08-11 17:05:19 +0800

    20230811

--- express_serve/index.js
+++ express_serve/index.js
@@ -1,47 +1,50 @@
-const express = require('express')
-const request = require('request')
+const express = require("express");
+const request = require("request");
 
-const app = express()
+const app = express();
 
 function delayTimer(time) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve()
-        }, time)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
 }
 
 /** 新闻列表 */
 function getNewsLHandle(last_oid) {
-    return new Promise((resolve) => {
-        request(
-            {
-                method: "get",
-                url: `https://opser.api.dgtle.com/v2/news/index?last_oid=${last_oid}`,
-            },
-            (err, response) => {
-                if (err) {
-                    resolve([])
-                }
-                resolve(response.body)
-            }
-        )
-    })
-} 
+  return new Promise((resolve) => {
+    request(
+      {
+        method: "get",
+        url: `https://opser.api.dgtle.com/v2/news/index?last_oid=${last_oid}`,
+      },
+      (err, response) => {
+        if (err) {
+          resolve([]);
+        }
+        resolve(response.body);
+      }
+    );
+  });
+}
 app.get("/news", async (req, res) => {
-    // const { last_oid = "" } = req.query;
-    // console.log('target', last_oid)
-    // await delayTimer(5000)
-    // const resolve = await getNewsLHandle(last_oid)
-    // res.send(resolve)
-    res.send(require('./news.json'))
+  // const { last_oid = "" } = req.query;
+  // console.log('target', last_oid)
+  // await delayTimer(5000)
+  // const resolve = await getNewsLHandle(last_oid)
+  // res.send(resolve)
+  res.send(require("./news.json"));
 });
 
-app.get("/live",(req,res)=>{
-    res.send(require('./live.json'))
-})
+app.get("/live", (req, res) => {
+  res.send(require("./live.json"));
+});
 
+app.get("/oneDayTalk", (req, res) => {
+  res.send(require("./oneDayTalk.json"));
+});
 
 app.listen(3000, () => {
-    console.log('service live in host 3000')
+  console.log("service live in host 3000");
 });
---
+++ firstApp/Views/OneDayTalkView.swift
@@ -0,0 +1,20 @@
+//
+//  OneDayTalkView.swift
+//  firstApp
+//
+//  Created by 蔡荣诠 on 2023/8/11.
+//
+
+import SwiftUI
+
+struct OneDayTalkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OneDayTalkView_Previews: PreviewProvider {
+    static var previews: some View {
+        OneDayTalkView()
+    }
+}
